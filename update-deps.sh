4e428b29d5b150719d43b0e0ecf65dc030828680
Benjie Gillam <benjie@jemjie.com>
2026-03-24 16:25:17 +0000

    rm @rc

--- update-deps.sh
+++ update-deps.sh
@@ -1,3 +1,3 @@
-yarn add @graphile-contrib/pg-many-to-many@rc @graphile/pg-aggregates@rc @graphile/simplify-inflection@rc graphile@rc postgraphile@rc postgraphile-plugin-connection-filter@rc @grafserv/persisted@rc @graphile-contrib/pg-omit-archived@rc @benjie/tsconfig
+yarn add @graphile-contrib/pg-many-to-many@rc @graphile/pg-aggregates@rc @graphile/simplify-inflection@rc graphile postgraphile postgraphile-plugin-connection-filter@rc @grafserv/persisted @graphile-contrib/pg-omit-archived@rc @benjie/tsconfig
 yarn dedupe
 
